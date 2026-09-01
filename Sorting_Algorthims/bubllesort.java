ceb2ab626a73b8c8a50952382d017500871d6737
Sankranthi <siva.sai.sankranthi@accenture.com>
2026-09-02 00:08:14 +0530

    02/09/2026

--- Sorting_Algorthims/bubllesort.java
+++ Sorting_Algorthims/bubllesort.java
@@ -2,7 +2,7 @@ public class bubllesort {
     public static void main(String[] args) {
         
     }
-     public static int[] bubbleSort(int[] nums) {
+     public static void bubbleSort(int[] nums) {
 
         for(int i=nums.length-1;i>=0;i--){
                 
---
+++ sliding wndow two pointers/longestsubstrringoptimal.java
@@ -0,0 +1,7 @@
+public class longestsubstrringoptimal {
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
